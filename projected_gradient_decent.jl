77e3ee77808a18c72b9c5d7b43efae49991386cf
Jon Lavington <jola2372@colorado.edu>
2019-04-22 19:24:17 -0700

    Update projected_gradient_decent.jl

--- projected_gradient_decent.jl
+++ projected_gradient_decent.jl
@@ -21,6 +21,21 @@ function computeBasis(x,n)
 	return B
 end
 
+function proj_step(B, C, alpha_b, step_size, g)
+	# first projected step
+	alpha_bNew = (B*inv(B'*B)*B')*(alpha_bNew - step_size * g)
+	# return all alpha that no longer satify constraints
+	alpha_lower = findall(0 .>  alpha_bNew)
+	alpha_higher = findall(C .<  alpha_bNew)
+	alpha_other = findall((alpha_bNew .> 0) .& (alpha_bNew.< C))
+	# set all these values
+	alpha_bNew[alpha_other] = (alpha_b - step_size * g)[alpha_other]
+	alpha_bNew[alpha_lower] = zeros(length(alpha_higher))
+	alpha_bNew[alpha_higher] = C*ones(length(alpha_higher))
+	# take the real projection step with clipped values
+	alpha_bNew = (B*inv(B'*B)*B')*(alpha_bNew)
+	return alpha_bNew
+end
 
 function fit(X, y, C, epsilon, max_iter, Numblocks)
     # Initializations
@@ -31,8 +46,9 @@ function fit(X, y, C, epsilon, max_iter, Numblocks)
     # set blocks
     blocks = reshape(randperm(length(y)), (Numblocks, Int(length(y) / Numblocks)))
 
-    # initial stepsize
+    # initial stepsize + sufficient decrease parameter
     step_size = 1
+	gamma = 1e-4
 
     # primary loop
     while true
@@ -50,24 +66,24 @@ function fit(X, y, C, epsilon, max_iter, Numblocks)
         alpha_b = alpha[block]
         n_b = length(block)
 
-	# get update info
-	(f, g, H) = objective(y, x, alpha)
+		# get objective info
+		(f_b, g_b, H_b) = objective(y_b, x_b, alpha_b)
+		# get basis
+		B = get_basis(alpha_b, n_b)
 
-	# get basis
-	B = get_basis(alpha_b, n_b)
+		# Try out the current step-size
+		alpha_bNew = proj_step(B, C, alpha_b, step_size, g)
+		(f_bNew, g_bNew, H_bNew) = objective(y_b, x_b, alpha_bNew)
 
-	# take gradient and use nullspace projection
-	alpha_bNew = (B*inv(B'*B)*B')*(x_bNew - step_size * g)
-
-	# return all alpha that no longer satify constraints
-	alpha_lower = findall(0 .>  alpha_bNew)
-	alpha_higher = findall(C .<  alpha_bNew)
-	alpha_other = findall((alpha_bNew .> 0) .& (alpha_bNew.< C))
-
-	# take the real projection step
-	alpha_b[alpha_lower] = (B*inv(B'*B)*B')*(zeros(length(alpha_higher)))
-	alpha_b[alpha_higher] = (B*inv(B'*B)*B')*(C*ones(length(alpha_higher)))
-	alpha_b[alpha_other] = alpha_bNew[alpha_other]
+		# Decrease the step-size if we increased the function
+		gg = dot(g_b,g_b)
+		while f_bNew > f_b - gamma*step_size*gg
+			# Fit a degree-2 polynomial to set step-size
+			step_size = step_size^2*gg/(2(f_bNew - f_b + step_size*gg))
+			# Try out the smaller step-size
+			alpha_bNew = proj_step(B, C, alpha_b, step_size, g_b)
+			(f_b, g_b, H_b) = objective(y_b, x_b, alpha_bNew)
+		end
 
         # Check convergence
         diff = norm(alpha - alpha_prev)
