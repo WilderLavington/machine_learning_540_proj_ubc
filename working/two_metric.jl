624a1aff1ee2be47ae20854ab7d8025d53d193db
JediJoey <joeyzhou19951208@gmail.com>
2019-11-24 23:44:57 -0800

    efficient two metric projection

--- working/two_metric.jl
+++ working/two_metric.jl
@@ -1,74 +1,77 @@
+function lambda_solver(d, a, c, lo, b)
+
+    breakpoints = unique(append!(a - d.*b,a - d.*lo))
+    breakpoints = sort(breakpoints)
+    l = 1
+    r = length(breakpoints)
+    L = sum(b)
+    R = sum(lo)
+
+    # Step 1
+    while true
+        if r-l == 1
+
+    # Step 4
+            return y[l]+(y[r]-y[l])*(c-L)/(R-L)
+        else
+            global m
+            m = floor((l+r)/2)
+        end
+
+    # Step 2
+        C = sum(max.(min.((a.-y[m])./d,b),l))
+
+    # Step 3
+        if C=c
+            return y[m]
+        elseif C>c
+            l = m
+            L = C
+        elseif C<c
+            r = m
+            R = C
+        end
+    end
+end
+
+function projection(d, a, c, lo, b)
+# This function find the projection of x onto the contraints
+# d is the diagonal of the quadratic function, a is the coefficient
+# c is the sum, lo is the lowerbound and b is the upperbound
+# see Helgason's paper for this algorithm
+    lammy = lambda_solver(d, a, c, lo, b)
+    n = length(d)
+    x = zeros(n)
+    for i = 1:n
+        if lammy <= a[i]-d[i]*b[i]
+            x[i] = b[i]
+        elseif a[i]-d[i]*b[i] < lammy < a[i]-d[i]*lo[i]
+            x[i] = (a[i]-lammy)/d[i]
+        else
+            x[i] = lo[i]
+        end
+    end
+    return x, lammy
+end
+
 function piecewise_linear(y,gradient,I1,I2)
-# a piecewise_linear equation solver, y is the label vector, gradient is
-# the gradient vector, I1 and I2 are index sets
-
-    function solve(lowerbound,upperbound)
-
-        leftside = 0
-        rightside = 0
-
-        for i = 1:length(y)
-            if i in I1
-                if y[i] == 1 && lowerbound >= gradient[i]
-                    leftside += 1
-                    rightside += y[i]*gradient[i]
-                elseif y[i] == -1 && upperbound =< -gradient[i]
-                    leftside += 1
-                    rightside += y[i]*gradient[i]
-                end
-            elseif i in I2
-                if y[i] == 1 && upperbound =< gradient[i]
-                    leftside += 1
-                    rightside += y[i]*gradient[i]
-                elseif y[i] == -1 && lowerbound >= -gradient[i]
-                    leftside += 1
-                    rightside += y[i]*gradient[i]
-                end
-            else
-                leftside +=1
-                rightside += y[i]*gradient[i]
-            end
+    n = length(y)
+    b = zeros(n) .+ Inf
+    lo = zeros(n) .- Inf
+    for i = 1:n
+        if in(I1).(i)
+            lo[i] = 0
         end
 
-        if leftside == 0
-            return (true, upperbound)
-        elseif rightside/leftside<lowerbound || rightside/leftside>upperbound
-            return (false, 0)
-        else
-            return (true, rightside/leftside)
+        if in(I2).(i)
+            b[i] = 0
         end
     end
 
-    index = Array(1:length(y))
-    breakpoints = (y.*gradient)[in(I1).(index) .| in(I2).(index)]
-    order = sort(breakpoints)
-    if length(order) == 0
-        order = [-Inf Inf]
-    else
-        order = [-Inf order Inf]
-    end
+    (d,lammy) = projection(y, -gradient, 0, lo, b)
 
-    for i in 1:length(order)-1
-        global lammy
-        (valid, lammy) = solve(order[i],order[i+1])
-        if valid == true
-            break
-        end
-    end
-
-    d = zeros(length(y))
-    for i = 1:length(y)
-        val = y[i]*lammy-gradient[i]
-        if i in I1
-            val > 0 ? d[i] = val : d[i] = 0
-        elseif i in I2
-            val < 0 ? d[i] = val : d[i] = 0
-        else
-            d[i]=val
-        end
-    end
-    I1t = index[in(I1).(index) .& y*lammy.<gradient]
-    I2t = index[in(I2).(index) .& y*lammy.>gradient]
+    I1t = index[in(I1).(index) .& y*lammy .< gradient]
+    I2t = index[in(I2).(index) .& y*lammy .> gradient]
     return (d, I1t, I2t)
 end
 
@@ -100,65 +103,4 @@ function two_metric_per_ite(X, y, C, alpha, H)
     (dxt, a, b) = piecewise_linear(ynew,-Dd,I1,I2)
     dxt[in(I1t).(index) .| in(I2t).(index)] .= 0
     return -(dxt+dxplus)
-end
-
-function projection(y, C, x)
-# This function find the projection of x onto the contraints
-    function solve(lowerbound,upperbound)
-
-        leftside = 0
-        rightside = 0
-
-        for i = 1:length(y)
-            if y[i] == 1 && (lowerbound >= -x[i] && upperbound <= -x[i]+C)
-                leftside += 1
-                rightside += -x[i]
-            else if y[i] == 1 && (lowerbound > -x[i]+C)
-                rightside -= C
-            elseif y[i] == -1 && (lowerbound >= x[i]-C && upperbound <= x[i])
-                leftside += 1
-                rightside += x[i]
-            elseif y[i] == -1 && (lowerbound > x[i])
-                rightside += C
-            end
-        end
-
-        if leftside == 0
-            return (true, upperbound)
-        elseif rightside/leftside<lowerbound || rightside/leftside>upperbound
-            return (false, 0)
-        else
-            return (true, rightside/leftside)
-        end
-    end
-
-    index = Array(1:length(y))
-    breakpoints = append!(y.*-x,y.*(C.-x))
-    order = sort(breakpoints)
-    if length(order) == 0
-        order = [-Inf Inf]
-    else
-        order = [-Inf order Inf]
-    end
-
-    for i in 1:length(order)-1
-        global lammy
-        (valid, lammy) = solve(order[i],order[i+1])
-        if valid == true
-            break
-        end
-    end
-
-    d = zeros(length(y))
-    for i = 1:length(y)
-        val = y[i]*lammy+x[i]
-        if val < 0
-            d[i] = 0
-        elseif val > C
-            d[i] = C
-        else
-            d[i]=val
-        end
-    end
-    return d
 end
