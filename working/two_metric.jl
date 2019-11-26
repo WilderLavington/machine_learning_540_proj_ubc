3778f4bf6239129e77f2522ee095c3484e3db942
JediJoey <joeyzhou19951208@gmail.com>
2019-11-26 00:06:15 -0800

    two metric for SMO

--- working/two_metric.jl
+++ working/two_metric.jl
@@ -1,9 +1,12 @@
-function lambda_solver(d, a, c, lo, b)
+using LinearAlgebra
 
-    breakpoints = unique(append!(a - d.*b,a - d.*lo))
+function lambda_solver(a, c, lo, b)
+
+    breakpoints = unique(append!(a - b,a - lo))
     breakpoints = sort(breakpoints)
+    y = breakpoints
     l = 1
-    r = length(breakpoints)
+    r = Int(length(breakpoints))
     L = sum(b)
     R = sum(lo)
 
@@ -15,76 +18,94 @@ function lambda_solver(d, a, c, lo, b)
             return y[l]+(y[r]-y[l])*(c-L)/(R-L)
         else
             global m
-            m = floor((l+r)/2)
+            m = Int(floor((l+r)/2))
         end
 
     # Step 2
-        C = sum(max.(min.((a.-y[m])./d,b),l))
+        C = sum(max.(min.(a.-y[m],b),lo))
 
     # Step 3
-        if C=c
+        if C == c
             return y[m]
-        elseif C>c
+        elseif C > c
             l = m
             L = C
-        elseif C<c
+        elseif C < c
             r = m
             R = C
         end
     end
 end
 
-function projection(d, a, c, lo, b)
+function projection(y, a, c, lo, b)
 # This function find the projection of x onto the contraints
-# d is the diagonal of the quadratic function, a is the coefficient
+# y is the coefficient of the linear constraint, a is the projecting point
 # c is the sum, lo is the lowerbound and b is the upperbound
 # see Helgason's paper for this algorithm
-    lammy = lambda_solver(d, a, c, lo, b)
-    n = length(d)
+    n = length(y)
+    index = Array(1:n)
+    minus = index[y.==-1]
+    a[minus] = -a[minus]
+    temp = lo
+    lo[minus] = -b[minus]
+    b[minus] = -temp[minus]
+
+    lammy = lambda_solver(a, c, lo, b)
+
     x = zeros(n)
     for i = 1:n
-        if lammy <= a[i]-d[i]*b[i]
+        if lammy <= a[i]-b[i]
             x[i] = b[i]
-        elseif a[i]-d[i]*b[i] < lammy < a[i]-d[i]*lo[i]
-            x[i] = (a[i]-lammy)/d[i]
+        elseif a[i]-b[i] < lammy < a[i]-lo[i]
+            x[i] = a[i]-lammy
         else
             x[i] = lo[i]
         end
     end
+    x[minus] = -x[minus]
     return x, lammy
 end
 
-function piecewise_linear(y,gradient,I1,I2)
+function piecewise_linear(y,gradient,I1,I2,I1til,I2til)
     n = length(y)
-    b = zeros(n) .+ Inf
-    lo = zeros(n) .- Inf
+    index = Array(1:n)
+    nor = norm(gradient)
+    b = zeros(n) .+ nor
+    lo = zeros(n) .- nor
     for i = 1:n
-        if in(I1).(i)
+        if i in I1
             lo[i] = 0
         end
 
-        if in(I2).(i)
+        if i in I2
+            b[i] = 0
+        end
+
+        if i in I1til || i in I2til
+            lo[i] = 0
             b[i] = 0
         end
     end
 
-    (d,lammy) = projection(y, -gradient, 0, lo, b)
+    (d,lammy) = projection(y, gradient, 0, lo, b)
 
-    I1t = index[in(I1).(index) .& y*lammy .< gradient]
-    I2t = index[in(I2).(index) .& y*lammy .> gradient]
+    lammy = -lammy
+
+    I1t = index[(in(I1).(index)) .& (y*lammy .< gradient)]
+    I2t = index[(in(I2).(index)) .& (y*lammy .> gradient)]
     return (d, I1t, I2t)
 end
 
 # This function computes a descent direction d
-function two_metric_per_ite(X, y, C, alpha, H)
+function two_metric_per_ite(y, gradient, C, alpha, H)
     # H is the Hessian
     # Step 1
-    n, d = size(X)
+    n = length(y)
     gradient = H * alpha - ones(size(y))
     index = Array(1:n)
     I1 = index[alpha.==0]
     I2 = index[alpha.==C]
-    (dx, I1t, I2t) = piecewise_linear(y,gradient,I1,I2)
+    (dx, I1t, I2t) = piecewise_linear(y,-gradient,I1,I2,[],[])
     # Step 2
     dxplus = -(dx+gradient)
     # Step 3
@@ -98,9 +119,6 @@ function two_metric_per_ite(X, y, C, alpha, H)
     end
     # Step 4
     Dd = D*dx
-    ynew = y
-    ynew[in(I1t).(index) .| in(I2t).(index)] .= 0
-    (dxt, a, b) = piecewise_linear(ynew,-Dd,I1,I2)
-    dxt[in(I1t).(index) .| in(I2t).(index)] .= 0
+    (dxt, ph1, ph2) = piecewise_linear(y,Dd,I1,I2,I1t,I2t)
     return -(dxt+dxplus)
 end
